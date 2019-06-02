776c043181eb8056f5783599599b7986810ad57f
denisson leal <denissonleal@gmail.com>
2019-06-02 18:52:47 -0300

    liga formulario com funcoes de acao

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+$(function() {
+	$('#new-command [name=operation]').change(function() {
+		let lock_field = $(this).val() != 'write_item';
+		$('#new-command [name=value]')
+			.prop('disabled', lock_field)
+			.closest('.box')
+			.toggle(!lock_field);
+	}).change();
+
+	$('#new-command').submit(function(ev) {
+		ev.preventDefault();
+		let transaction = $('#new-command [name=transaction]').val();
+		let operation = $('#new-command [name=operation]').val();
+		let variable = $('#new-command [name=variable]').val();
+		let value = $('#new-command [name=value]').val();
+
+		let next = false;
+		switch(operation) {
+			case 'read_lock':
+				next = read_lock(transaction, variable);
+				break;
+			case 'write_lock':
+				next = write_lock(transaction, variable);
+				break;
+			case 'unlock':
+				next = unlock(transaction, variable);
+				break;
+			case 'read_item':
+				next = can_read(transaction, variable);
+				break;
+			case 'write_item':
+				next = can_write(transaction, variable);
+				if (next) {
+					edit_memory(variable, value);
+				}
+				break;
+		}
+		if (next) {
+			add_logs(transaction, operation, variable, value)
+		}
+	})
+});
 
 function edit_memory(variable, value) {
 	let selector = $(`#memory tr[data=${variable}]`);
@@ -12,15 +54,15 @@ function delete_memory(variable) {
 	$(`#memory tr[data=${variable}]`).remove();
 }
 
-function add_logs(log) {
+function add_logs(transaction, operation, variable, value) {
 
 	var txt_write = ''
-	if (log.commad == 'write') {
-		txt_write = `, ${log.value}`;
+	if (operation == 'write') {
+		txt_write = `, ${value}`;
 	}
 
 	$('#logs').append(`<div class="item-log">
-		${log.transaction}: ${log.commad}(${log.variable}${txt_write})
+		${transaction}: ${operation}(${variable}${txt_write})
 	</div>`);
 }
 
@@ -28,6 +70,7 @@ function add_logs(log) {
 function write_lock(transaction, variable) {
 	let selector = $(`#lock tr[variable=${variable}]`);
 	if (selector.length) {
+		alert('A variável já possui um bloqueio');
 		return false;
 	}
 
@@ -56,6 +99,7 @@ function read_lock(transaction, variable) {
 
 	// quando ja possui um bloqueio de escrita
 	if (selector.find('.lock-type').html() == 'WRITE') {
+		alert('A variável já possui um bloqueio de escrita');
 		return false;
 	}
 
@@ -66,6 +110,7 @@ function read_lock(transaction, variable) {
 		.split(', ');
 
 	if (lock_transactions.indexOf(transaction) != -1) {
+		alert('Essa transação já possui esse bloqueio');
 		return false;
 	}
 
@@ -81,6 +126,7 @@ function read_lock(transaction, variable) {
 function can_read(transaction, variable) {
 	let selector = $(`#lock tr[variable=${variable}]`);
 	if (!selector.length) {
+		alert('A variável não está bloqueada');
 		return false;
 	}
 
@@ -90,6 +136,7 @@ function can_read(transaction, variable) {
 		.split(', ');
 
 	if (lock_transactions.indexOf(transaction) == -1) {
+		alert('A transação não bloqueou a variável');
 		return false;
 	}
 
@@ -99,11 +146,7 @@ function can_read(transaction, variable) {
 function can_write(transaction, variable) {
 	let selector = $(`#lock tr[variable=${variable}]`);
 	if (!selector.length) {
-		return false;
-	}
-
-	let lock_type = selector.find('.lock-type').html();
-	if (lock_type !== 'WRITE') {
+		alert('A variável não está bloqueada');
 		return false;
 	}
 
@@ -113,6 +156,13 @@ function can_write(transaction, variable) {
 		.split(', ');
 
 	if (lock_transactions.indexOf(transaction) == -1) {
+		alert('A transação não bloqueou a variável');
+		return false;
+	}
+
+	let lock_type = selector.find('.lock-type').html();
+	if (lock_type !== 'WRITE') {
+		alert('A transação não bloqueou a variável para escrita');
 		return false;
 	}
 
@@ -122,6 +172,7 @@ function can_write(transaction, variable) {
 function unlock(transaction, variable) {
 	let selector = $(`#lock tr[variable=${variable}]`);
 	if (!selector.length) {
+		alert('A variável não está bloqueada');
 		return false;
 	}
 
@@ -131,6 +182,7 @@ function unlock(transaction, variable) {
 		.split(', ');
 
 	if (lock_transactions.indexOf(transaction) == -1) {
+		alert('Essa transação não bloqueou essa variável');
 		return false;
 	}
 
