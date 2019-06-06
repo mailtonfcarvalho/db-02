1b2ba183850a76c16dbe55566b1c4157b1b931c9
denisson leal <denissonleal@gmail.com>
2019-06-06 14:10:11 -0300

    add funcao de verificacao de transacao com item na memoria

--- js/memory-ctrl.js
+++ js/memory-ctrl.js
@@ -3,10 +3,14 @@ function edit_memory(transaction, variable, value) {
 	if (selector.length) {
 		selector.find('.memory-value').html(value);
 	} else {
-		$('#memory').append(`<tr data=${variable}><td id=${transaction}>${transaction}</td><td>${variable}</td><td class="memory-value">${value}</td></tr>`);
+		$('#memory').append(`<tr data=${variable} data-transaction=${transaction}><td>${transaction}</td><td>${variable}</td><td class="memory-value">${value}</td></tr>`);
 	}
 }
 
 function delete_memory(variable) {
 	$(`#memory tr[data=${variable}]`).remove();
+}
+
+function has_items_in_memory(transaction) {
+	return $(`#memory tr[data-transaction=${transaction}]`).length > 0;
 }
