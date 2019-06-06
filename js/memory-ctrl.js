5cf6656cd80c43be3fee3db4e475f2ad566550ec
denisson leal <denissonleal@gmail.com>
2019-06-06 17:14:03 -0300

    comenta funcoes do memory-ctrl. Closes #6

--- js/memory-ctrl.js
+++ js/memory-ctrl.js
@@ -1,24 +1,48 @@
+/**
+ * edita valor de uma variavel em memoria
+ */
 function edit_memory(transaction, variable, value) {
 	let selector = $(`#memory tr[data=${variable}]`);
 	if (selector.length) {
 		selector.find('.memory-value').html(value);
 	} else {
-		$('#memory').append(`<tr data=${variable} data-transaction=${transaction}><td>${transaction}</td><td>${variable}</td><td class="memory-value">${value}</td></tr>`);
+		$('#memory').append(
+			`<tr data=${variable} data-transaction=${transaction}>
+				<td>${transaction}</td>
+				<td>${variable}</td>
+				<td class="memory-value">${value}</td>
+			</tr>`
+		);
 	}
 }
 
+/**
+ * apaga uma variavel na memoria
+ */
 function delete_memory(variable) {
 	$(`#memory tr[data=${variable}]`).remove();
 }
 
+/**
+ * apaga toda a memoria de uma trasacao
+ */
 function delete_items_transaction_in_memory(transaction) {
 	$(`#memory tr[data-transaction=${transaction}]`).remove();
 }
 
+/**
+ * verifica se uma trasacao tem dados em memoria
+ */
 function has_items_in_memory(transaction) {
 	return $(`#memory tr[data-transaction=${transaction}]`).length > 0;
 }
 
+/**
+ * realiza o commit de uma transacao
+ * - pega as variaveis de uma transacao que estao na memoria
+ * - manda salva no disco
+ * - apada da memoria
+ */
 function commit_memory(transaction) {
 	let selector = $(`#memory tr[data-transaction=${transaction}]`);
 	for (var i = 0; i < selector.length; i++) {
