8c9e670c3b30c4f252e1597c2aeb3f35727aa036
denisson leal <denissonleal@gmail.com>
2019-06-06 13:37:19 -0300

    coloca funcoes de verificacao de transacao nas operacoes

--- js/command.js
+++ js/command.js
@@ -31,22 +31,70 @@ $(function() {
 		let next = false;
 		switch(operation) {
 			case 'read_lock':
-				next = read_lock(transaction, variable);
+				next = has_transaction(transaction);
+				if (next) {
+					next = can_transaction(transaction, 'locking');
+				}
+				if (next) {
+					next = read_lock(transaction, variable);
+				}
 				break;
+
 			case 'write_lock':
-				next = write_lock(transaction, variable);
+				next = has_transaction(transaction);
+				if (next) {
+					next = can_transaction(transaction, 'locking');
+				}
+				if (next) {
+					next = write_lock(transaction, variable);
+				}
 				break;
+
 			case 'unlock':
-				next = unlock(transaction, variable);
+				next = has_transaction(transaction);
+				if (next) {
+					change_transaction(transaction, 'unlocking');
+					next = unlock(transaction, variable);
+				}
 				break;
+
 			case 'read_item':
-				next = can_read(transaction, variable);
+				next = has_transaction(transaction);
+				if (next) {
+					next = can_read(transaction, variable);
+				}
 				break;
+
 			case 'write_item':
-				next = can_write(transaction, variable);
+				next = has_transaction(transaction);
+				if (next) {
+					next = can_write(transaction, variable);
+				}
 				if (next) {
 					edit_memory(transaction,variable, value);
 				}
+				break;
+
+			case 'begin_transaction':
+				next = begin_transaction(transaction);
+				break;
+
+			case 'end_transaction':
+				// verificar se a transacao tem bloqueio ativo... [implementar]
+				next = end_transaction(transaction);
+				break;
+
+			case 'commit':
+				// verifica se existe transacao
+				// verifica estado da transacao
+				// passar dados para o disco
+				break;
+
+			case 'abort':
+				// verifica se existe transacao
+				// apaga tudo da transacao do locking table
+				// apaga tudo da transacao na memoria
+				// apaga a transacao
 				break;
 		}
 		if (next) {
