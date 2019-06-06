3c34f2a15882e4e6a0227c8f8eaff2706340b4d5
denisson leal <denissonleal@gmail.com>
2019-06-06 13:32:20 -0300

    verifica tipos de log que nao possuem variaveis

--- js/log-ctrl.js
+++ js/log-ctrl.js
@@ -5,6 +5,13 @@ function add_logs(transaction, operation, variable, value) {
 		txt_write = `, ${value}`;
 	}
 
+	variable = [
+		'begin_transaction',
+		'commit',
+		'abort',
+		'end_transaction',
+	].indexOf(operation) == -1 ? variable : '';
+
 	$('#logs').append(`<div class="item-log">
 		${transaction}: ${operation}(${variable}${txt_write})
 	</div>`);
